3b01bf2a82afcea35b961a45452251dd4d1d5edd
Tes Wurgie <tesogi2006@gmail.com>
2025-11-06 05:35:02 -0500

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './routes/Home';
 import Play from './routes/Play';
 import './styles/globals.css';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play/:id" element={<Play />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
